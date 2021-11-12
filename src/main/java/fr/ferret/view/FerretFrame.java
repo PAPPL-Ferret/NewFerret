caf6eb15a1a5bd0fef0123cdf15e0545d4640166
agathecheri <82039936+agathecheri@users.noreply.github.com>
2021-11-12 16:14:42 +0100

    ajout du RunPanel + la fenêtre de choix de sauvegarde

--- src/main/java/fr/ferret/view/FerretFrame.java
+++ src/main/java/fr/ferret/view/FerretFrame.java
@@ -3,6 +3,7 @@ package fr.ferret.view;
 import fr.ferret.view.panel.HeaderPanel;
 import fr.ferret.view.panel.LocusPanel;
 import fr.ferret.view.panel.RegionPanel;
+import fr.ferret.view.panel.RunPanel;
 
 import javax.swing.*;
 
@@ -11,24 +12,27 @@ public class FerretFrame extends JFrame
     private final HeaderPanel headerPanel;
     private final LocusPanel locusPanel;
     private final RegionPanel regionPanel;
+    private final RunPanel runPanel;
 
     public FerretFrame() {
         headerPanel = new HeaderPanel();
         locusPanel = new LocusPanel();
         regionPanel = new RegionPanel();
+        runPanel = new RunPanel();
 
         setTitle("Ferret v3");
 
         JPanel panel = new JPanel();
         panel.add(locusPanel);
         panel.add(regionPanel);
+        panel.add(runPanel);
 
         // affecte le panneau a la fenetre
         setContentPane(panel);
         pack();
 
         //maFrame.add(new JButton("Button 1")); //SwingConstants.CENTER
-        setSize(800,800);
+        setSize(800,725);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
