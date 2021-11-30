108410a22a8df008dddaf43477d6383e4e06b8b1
agathecheri <82039936+agathecheri@users.noreply.github.com>
2021-11-30 15:11:28 +0100

    création des classes VariantPanel et GenePanel + ajout des onglets sur le layout global

--- src/main/java/fr/ferret/view/FerretFrame.java
+++ src/main/java/fr/ferret/view/FerretFrame.java
@@ -1,17 +1,17 @@
 package fr.ferret.view;
 
 import fr.ferret.FerretTest;
+import fr.ferret.view.panel.GenePanel;
 import fr.ferret.view.panel.header.MenuPanel;
 import fr.ferret.view.panel.LocusPanel;
 import fr.ferret.view.panel.RegionPanel;
 import fr.ferret.view.panel.RunPanel;
+import fr.ferret.view.panel.VariantPanel;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
 import java.io.IOException;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Main Ferret frame
@@ -22,6 +22,8 @@ public class FerretFrame extends JFrame
     private final LocusPanel locusPanel;
     private final RegionPanel regionPanel;
     private final RunPanel runPanel;
+    private final GenePanel genePanel;
+    private final VariantPanel variantPanel;
 
     public FerretFrame() {
         //Set look
@@ -39,15 +41,26 @@ public class FerretFrame extends JFrame
 
         headerPanel = new MenuPanel(this);
         locusPanel = new LocusPanel();
+        genePanel = new GenePanel();
+        variantPanel = new VariantPanel();
         regionPanel = new RegionPanel();
         runPanel = new RunPanel(this);
+
+        //Créer le conteneur des onglets
+        JTabbedPane onglets = new JTabbedPane();
+        //Définir la position de conteneur d'onglets
+        onglets.setBounds(40,20,300,300);
+        //Associer chaque panneau à l'onglet correspondant
+        onglets.add("Locus", locusPanel);
+        onglets.add("Gene", genePanel);
+        onglets.add("Variant", variantPanel);
 
         setTitle("Ferret v3");
         setJMenuBar(headerPanel);
 
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(locusPanel);
+        panel.add(onglets);
         panel.add(regionPanel);
         panel.add(runPanel);
 
